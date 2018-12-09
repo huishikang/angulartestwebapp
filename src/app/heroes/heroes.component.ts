726bc5a977887899e927a5ca459480b16a4b4a60
747021484 <747021484a@gmail.com>
2018-12-09 19:36:50 +0800

    message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,12 +8,16 @@ import { Hero } from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
   constructor() { }
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+  selectedHero: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
   //hero = 'Windstorm';
   ngOnInit() {
     
